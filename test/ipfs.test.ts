b2eec2be60d4633e9d20fd91570e484e76487e7c
fly33499 <fly33499@gmail.com>
2021-09-29 21:23:45 +0900

    add ipfs test module & sample file

---
+++ test/ipfs.test.ts
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { FirmaSDK } from "../sdk/FirmaSDK"
+import { FirmaConfig } from "../sdk/FirmaConfig"
+
+describe('[IPFS Test]', () => {
+
+	const firma = new FirmaSDK(FirmaConfig.LocalDevNetConfig);;
+
+	it('ipfs test', async () => {
+
+		var hash = await firma.Ipfs.addJson("hello world");
+		expect(hash).to.equal("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD");
+
+		var ee = new ArrayBuffer(1000);
+		var hash = await firma.Ipfs.addBuffer(ee);
+		var url = firma.Ipfs.getURLFromHash(hash);
+
+		expect(url).to.equal("https://ipfs-firma-devnet.firmachain.org/ipfs/QmVRqQTWMy2gNtNd8i9ugz8STaoZmFGYg6fn5YyEBHp9Be");
+
+		hash = await firma.Ipfs.addFile("./test/sample/test-bear.jpg");
+		url = firma.Ipfs.getURLFromHash(hash);
+
+		expect(url).to.equal("https://ipfs-firma-devnet.firmachain.org/ipfs/QmYsezxzunake9EmyoU4HsWKEyHQLgE3syTEpTSQEhNChA");
+	});
+});
