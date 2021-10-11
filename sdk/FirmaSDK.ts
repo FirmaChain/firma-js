42c512e75373bc1e1bb64580b32440fe43abae2e
gomztest <fly33499@gmail.com>
2021-10-11 11:03:42 +0900

    feat: update firmaSDK for support staking, distribution, gov services

--- sdk/FirmaSDK.ts
+++ sdk/FirmaSDK.ts
@@ -1,21 +1,28 @@
 import { FirmaConfig } from "./FirmaConfig";
 import { FirmaBankService } from "./FirmaBankService";
 import { FirmaFeeGrantService } from "./FirmaFeeGrantService";
+import { FirmaStakingService } from "./FirmaStakingService";
+import { FirmaDistributionService } from "./FirmaDistributionService";
 import { NftService } from "./FirmaNftService";
 import { ContractService } from "./FirmaContractService";
 import { IpfsService } from "./FirmaIpfsService";
 import { FirmaWalletService } from "./FirmaWalletService";
 import { FirmaUtil } from "./FirmaUtil";
+import { FirmaGovService } from "./FirmaGovService";
 
 export class FirmaSDK {
 	constructor(public Config: FirmaConfig,
 		public Wallet: FirmaWalletService = new FirmaWalletService(Config),
 		public Bank: FirmaBankService = new FirmaBankService(Config),
 		public FeeGrant: FirmaFeeGrantService = new FirmaFeeGrantService(Config),
+		public Staking: FirmaStakingService = new FirmaStakingService(Config),
+		public Distribution: FirmaDistributionService = new FirmaDistributionService(Config),
+		public Gov: FirmaGovService = new FirmaGovService(Config),
 		public Nft: NftService = new NftService(Config),
 		public Contract: ContractService = new ContractService(Config),
 		public Ipfs: IpfsService = new IpfsService(Config)) {
 			FirmaUtil.Config = Config;
 		 }
 }
+
 
