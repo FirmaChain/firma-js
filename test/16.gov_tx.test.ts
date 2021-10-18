585c240d28b4a8059b335607686cd4837f21105c
fly33499 <fly33499@gmail.com>
2021-10-18 21:31:57 +0900

    testcase: update gov tx for cancelsoftwareupgrade

--- test/16.gov_tx.test.ts
+++ test/16.gov_tx.test.ts
@@ -74,7 +74,7 @@ describe.skip('[16. Gov Tx Test]', () => {
 		const aliceWallet = await firma.Wallet.fromMnemonic(aliceMnemonic);
 
 		const proposer = await aliceWallet.getAddress();
-		const initialDepositFCT = 1000;
+		const initialDepositFCT = 10000;
 		const title = "Software Upgrade proposal1";
 		const description = "This is a software upgrade proposal";
 
@@ -83,10 +83,24 @@ describe.skip('[16. Gov Tx Test]', () => {
 
 		var result = await firma.Gov.submitSoftwareUpgradeProposalByHeight(aliceWallet, title, description, initialDepositFCT, proposer, upgradeName, upgradeHeight);
 
-		//console.log(result);
+		console.log(result);
 		expect(result.code).to.equal(0);
 	});
 
+	it('SubmitCancelSoftwareUpgradeProposal Test', async () => {
+
+		const aliceWallet = await firma.Wallet.fromMnemonic(aliceMnemonic);
+
+		const proposer = await aliceWallet.getAddress();
+		const initialDepositFCT = 1000;
+		const title = "Software Upgrade proposal1";
+		const description = "This is a software upgrade proposal";
+
+		var result = await firma.Gov.submitCancelSoftwareUpgradeProposal(aliceWallet, title, description, initialDepositFCT, proposer);
+
+		console.log(result);
+		expect(result.code).to.equal(0);
+	});
 
 	// NOTICE: time-based upgrades have been deprecated in the SDK: invalid request
 	/*it.skip('SubmitSoftwareUpgradeProposalByTime Test', async () => {
