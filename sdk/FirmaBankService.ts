2cb9c661df7da2191139f2e0e3af12192bde0a4c
fly33499 <fly33499@gmail.com>
2021-11-08 14:56:05 +0900

    feat: add sendToken, token query on bank module

--- sdk/FirmaBankService.ts
+++ sdk/FirmaBankService.ts
@@ -1,14 +1,15 @@
-import { BankTxClient, BankQueryClient, TxMisc, DefaultTxMisc, getSignAndBroadcastOption } from "./firmachain/bank";
+import { BankTxClient, BankQueryClient, TxMisc, DefaultTxMisc, getSignAndBroadcastOption, Token } from "./firmachain/bank";
 import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 
 import { FirmaWalletService } from "./FirmaWalletService";
 import { FirmaConfig } from "./FirmaConfig";
 import { FirmaUtil } from "./FirmaUtil";
 import { BroadcastTxResponse } from "./firmachain/common/stargateclient";
+import { TokenQueryClient } from "./firmachain/token";
 
 export class FirmaBankService {
 
-    constructor(private readonly config: FirmaConfig) {}
+    constructor(private readonly config: FirmaConfig) { }
 
     async getGasEstimationSend(wallet: FirmaWalletService,
         targetAddress: string,
@@ -16,8 +17,23 @@ export class FirmaBankService {
         txMisc: TxMisc = DefaultTxMisc): Promise<number> {
 
         try {
-            const txRaw = await this.getSignedTxSend(wallet, targetAddress, amount, txMisc);
+            const txRaw = await this.getSignedTxSend(wallet, targetAddress, this.config.denom, FirmaUtil.getUFCTStringFromFCT(amount), txMisc);
             return await FirmaUtil.estimateGas(txRaw);
+
+        } catch (error) {
+            FirmaUtil.printLog(error);
+            throw error;
+        }
+    }
+
+    async sendToken(wallet: FirmaWalletService, targetAddress: string, tokenID: string, amount: number, decimal: number, txMisc: TxMisc = DefaultTxMisc):
+        Promise<BroadcastTxResponse> {
+        try {
+
+            const txRaw = await this.getSignedTxSend(wallet, targetAddress, tokenID, FirmaUtil.getUTokenStringFromToken(amount, decimal), txMisc);
+
+            const bankTxClient = new BankTxClient(wallet.getRawWallet(), this.config.rpcAddress);
+            return await bankTxClient.broadcast(txRaw);
 
         } catch (error) {
             FirmaUtil.printLog(error);
@@ -28,10 +44,47 @@ export class FirmaBankService {
     async send(wallet: FirmaWalletService, targetAddress: string, amount: number, txMisc: TxMisc = DefaultTxMisc):
         Promise<BroadcastTxResponse> {
         try {
-            const txRaw = await this.getSignedTxSend(wallet, targetAddress, amount, txMisc);
+            const txRaw = await this.getSignedTxSend(wallet, targetAddress, this.config.denom, FirmaUtil.getUFCTStringFromFCT(amount), txMisc);
 
             const bankTxClient = new BankTxClient(wallet.getRawWallet(), this.config.rpcAddress);
             return await bankTxClient.broadcast(txRaw);
+
+        } catch (error) {
+            FirmaUtil.printLog(error);
+            throw error;
+        }
+    }
+
+    async getTokenBalanceList(address: string): Promise<Token[]> {
+        try {
+            const bankQueryClient = new BankQueryClient(this.config.restApiAddress);
+            const result = await bankQueryClient.queryBalanceList(address);
+
+            let tokenList: Token[] = [];
+
+            for (let i = 0; i < result.length; i++) {
+
+                // ignore config.denom
+                if (result[i].denom === this.config.denom)
+                    continue;
+
+                tokenList.push(result[i]);
+            }
+
+            return tokenList;
+
+        } catch (error) {
+            FirmaUtil.printLog(error);
+            throw error;
+        }
+    }
+
+    async getTokenBalance(address: string, tokenID: string): Promise<string> {
+        try {
+            const bankQueryClient = new BankQueryClient(this.config.restApiAddress);
+            const result = await bankQueryClient.queryTokenBalance(address, tokenID);
+
+            return result.amount;
 
         } catch (error) {
             FirmaUtil.printLog(error);
@@ -44,7 +97,7 @@ export class FirmaBankService {
             const bankQueryClient = new BankQueryClient(this.config.restApiAddress);
             const result = await bankQueryClient.queryBalance(address, this.config.denom);
 
-            return result;
+            return result.amount;
 
         } catch (error) {
             FirmaUtil.printLog(error);
@@ -52,16 +105,16 @@ export class FirmaBankService {
         }
     }
 
-
     private async getSignedTxSend(wallet: FirmaWalletService,
         targetAddress: string,
-        amount: number,
+        denom: string,
+        amount: string,
         txMisc: TxMisc = DefaultTxMisc): Promise<TxRaw> {
         try {
             const bankTxClient = new BankTxClient(wallet.getRawWallet(), this.config.rpcAddress);
 
             const address = await wallet.getAddress();
-            const sendAmount = { denom: this.config.denom, amount: FirmaUtil.getUFCTStringFromFCT(amount) };
+            const sendAmount = { denom: denom, amount: amount };
             const message = bankTxClient.msgSend({ fromAddress: address, toAddress: targetAddress, amount: [sendAmount] });
 
             return await bankTxClient.sign([message], getSignAndBroadcastOption(this.config.denom, txMisc));
@@ -71,6 +124,4 @@ export class FirmaBankService {
             throw error;
         }
     }
-
-
 }
