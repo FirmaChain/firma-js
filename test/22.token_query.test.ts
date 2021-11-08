6163b0afeb255b75d6e26c9ab63c9f12243ff77c
fly33499 <fly33499@gmail.com>
2021-11-08 14:58:09 +0900

    testcase: add token tx & query case

---
+++ test/22.token_query.test.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import { FirmaSDK } from "../sdk/FirmaSDK"
+import { aliceMnemonic, bobMnemonic, TestChainConfig } from './config_test';
+
+describe('[22. Token query Test]', () => {
+
+	const firma = new FirmaSDK(TestChainConfig);
+
+	it('Token getTokenDataListFromOwner', async () => {
+
+		const address = await (await firma.Wallet.fromMnemonic(aliceMnemonic)).getAddress();
+		var tokenDataList = await firma.Token.getTokenDataListFromOwner(address);
+		//console.log(totalData);
+		expect(tokenDataList.length).to.be.greaterThan(0);
+	});
+
+	it('Token getTokenData', async () => {
+
+		const address = await (await firma.Wallet.fromMnemonic(aliceMnemonic)).getAddress();
+		var tokenDataList = await firma.Token.getTokenDataListFromOwner(address);
+
+		const tokenID = tokenDataList[0];
+
+		var totalData = await firma.Token.getTokenData(tokenID);
+		//console.log(totalData);
+
+		expect(totalData.tokenID).to.be.equal(tokenID);
+	});
+
+	it('Token getTokenDataAll', async () => {
+
+		var totalDataList = await firma.Token.getGetTokenDataAll();
+		//console.log(totalDataList.dataList);
+		//console.log(totalDataList.pagination);
+
+		expect(totalDataList.dataList.length).to.be.greaterThan(0);
+		//expect(totalNft).to.be.greaterThan(0);
+	});
+});
