61e599c400b09142528d4c68a22409aeb0092786
fly33499 <fly33499@gmail.com>
2021-12-09 21:42:07 +0900

    feat: add firma-js mobile sdk

---
+++ sdk/FirmaMobileSDK.ts
@@ -0,0 +1,33 @@
+import { FirmaConfig } from "./FirmaConfig";
+import { FirmaBankService } from "./FirmaBankService";
+import { FirmaFeeGrantService } from "./FirmaFeeGrantService";
+import { FirmaStakingService } from "./FirmaStakingService";
+import { FirmaDistributionService } from "./FirmaDistributionService";
+import { NftService } from "./FirmaNftService";
+import { TokenService } from "./FirmaTokenService";
+import { ContractService } from "./FirmaContractService";
+import { FirmaWalletService } from "./FirmaWalletService";
+import { FirmaUtil } from "./FirmaUtil";
+import { FirmaGovService } from "./FirmaGovService";
+import { ChainService } from "./FirmaChainService";
+import { SlashingService } from "./FirmaSlashingService";
+
+export class FirmaMobileSDK {
+    constructor(public Config: FirmaConfig,
+        public Wallet: FirmaWalletService = new FirmaWalletService(Config),
+        public Bank: FirmaBankService = new FirmaBankService(Config),
+        public FeeGrant: FirmaFeeGrantService = new FirmaFeeGrantService(Config),
+        public Staking: FirmaStakingService = new FirmaStakingService(Config),
+        public Distribution: FirmaDistributionService = new FirmaDistributionService(Config),
+        public Gov: FirmaGovService = new FirmaGovService(Config),
+        public Nft: NftService = new NftService(Config),
+        public Token: TokenService = new TokenService(Config),
+        public Contract: ContractService = new ContractService(Config),
+        public BlockChain: ChainService = new ChainService(Config),
+        public Slashing: SlashingService = new SlashingService(Config)) {
+
+        FirmaUtil.config = Config;
+    }
+}
+
+
