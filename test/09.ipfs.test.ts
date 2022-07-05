7e3809a0284682c2d0e9049f958d91e4d6d120cb
Donghoon Kim <fly33499@gmail.com>
2022-07-05 21:42:55 +0900

    misc: ipfs module update

--- test/09.ipfs.test.ts
+++ test/09.ipfs.test.ts
@@ -16,7 +16,6 @@ describe('[09. IPFS Test]', () => {
 		var url = firma.Ipfs.getURLFromHash(hash);
 
 		let srcUrl = firma.Config.ipfsWebApiAddress + "/ipfs/QmVRqQTWMy2gNtNd8i9ugz8STaoZmFGYg6fn5YyEBHp9Be";
-
 		expect(srcUrl).to.equal(url);
 
 		hash = await firma.Ipfs.addFile("./test/sample/test-bear.jpg");
