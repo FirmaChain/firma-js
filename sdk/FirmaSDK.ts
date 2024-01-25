7741289a11163878b1571cb93758fc8d26f38bdb
fly33499 <fly33499@gmail.com>
2024-01-25 22:51:28 +0900

    feat: add cosmwasm bridge service

--- sdk/FirmaSDK.ts
+++ sdk/FirmaSDK.ts
@@ -18,6 +18,7 @@ import { FirmaCosmWasmService } from "./FirmaCosmWasmService";
 import { FirmaIbcService } from "./FirmaIbcService";
 import { FirmaMintService } from "./FirmaMintService";
 import { FirmaCosmWasmCw721Service } from "./FirmaCosmWasmCw721";
+import { FirmaCosmWasmCwBridgeService } from "./FirmaCosmWasmCwBridge";
 
 export class FirmaSDK {
     constructor(public Config: FirmaConfig,
@@ -39,6 +40,7 @@ export class FirmaSDK {
         public Mint: FirmaMintService = new FirmaMintService(Config),
         public Cw20: FirmaCosmWasmCw20Service = new FirmaCosmWasmCw20Service(Config, CosmWasm),
         public Cw721: FirmaCosmWasmCw721Service = new FirmaCosmWasmCw721Service(Config, CosmWasm),
+        public CwBridge: FirmaCosmWasmCwBridgeService = new FirmaCosmWasmCwBridgeService(Config, CosmWasm, Cw721),
     ) {
 
         FirmaUtil.config = Config;
