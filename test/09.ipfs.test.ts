4232cf6788556c3bac21898dac45b928b2e7c3b0
thismars <yhklip990@gmail.com>
2025-06-24 14:55:48 +0900

    refactor:Renaming variables for better readability

--- test/09.ipfs.test.ts
+++ test/09.ipfs.test.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { FirmaSDK } from '../sdk/FirmaSDK';
+
 import { TestChainConfig } from './config_test';
 
 describe('[09. IPFS Test]', () => {
@@ -12,12 +13,12 @@ describe('[09. IPFS Test]', () => {
 
 	it('ipfs test', async () => {
 
-		var hash = await firma.Ipfs.addJson("hello world");
+		let hash = await firma.Ipfs.addJson("hello world");
 		expect(hash).to.equal("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD");
 
-		var ee = new ArrayBuffer(1000);
-		var hash = await firma.Ipfs.addBuffer(ee);
-		var url = firma.Ipfs.getURLFromHash(hash);
+		const arrBuf = new ArrayBuffer(1000);
+		hash = await firma.Ipfs.addBuffer(arrBuf);
+		let url = firma.Ipfs.getURLFromHash(hash);
 
 		let srcUrl = firma.Config.ipfsWebApiAddress + "/ipfs/QmVRqQTWMy2gNtNd8i9ugz8STaoZmFGYg6fn5YyEBHp9Be";
 		expect(srcUrl).to.equal(url);
