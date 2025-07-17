497c3760567eeb5513132112b2fee1214f4fad48
thismars <yhklip990@gmail.com>
2025-07-17 12:51:52 +0900

    refactor: migrate from amino to protobuf

--- sdk/firmachain/common/accounts.ts
+++ sdk/firmachain/common/accounts.ts
@@ -1,31 +1,31 @@
 import { Pubkey } from "@cosmjs/amino";
 import { Uint64 } from "@cosmjs/math";
-import { decodePubkey } from "@cosmjs/proto-signing";
+import { decodeOptionalPubkey } from "@cosmjs/proto-signing";
+import { assert } from "@cosmjs/utils";
 import { BaseAccount, ModuleAccount } from "cosmjs-types/cosmos/auth/v1beta1/auth";
 import {
-    BaseVestingAccount,
-    ContinuousVestingAccount,
-    DelayedVestingAccount,
-    PeriodicVestingAccount,
+  BaseVestingAccount,
+  ContinuousVestingAccount,
+  DelayedVestingAccount,
+  PeriodicVestingAccount,
 } from "cosmjs-types/cosmos/vesting/v1beta1/vesting";
-import { Any } from "../google/protobuf/any";
-import Long from "long";
+import { Any } from "cosmjs-types/google/protobuf/any";
 
 export interface Account {
-    /** Bech32 account address */
-    readonly address: string;
-    readonly pubkey: Pubkey | null;
-    readonly accountNumber: number;
-    readonly sequence: number;
+  /** Bech32 account address */
+  readonly address: string;
+  readonly pubkey: Pubkey | null;
+  readonly accountNumber: number;
+  readonly sequence: number;
 }
 
-function uint64FromProto(input: number | Long): Uint64 {
+function uint64FromProto(input: number | bigint): Uint64 {
     return Uint64.fromString(input.toString());
 }
 
 function accountFromBaseAccount(input: BaseAccount): Account {
     const { address, pubKey, accountNumber, sequence } = input;
-    const pubkey = decodePubkey(pubKey);
+    const pubkey = decodeOptionalPubkey(pubKey);
     return {
         address: address,
         pubkey: pubkey,
@@ -35,53 +35,53 @@ function accountFromBaseAccount(input: BaseAccount): Account {
 }
 
 /**
- * Takes an `Any` encoded account from the chain and extracts some common
- * `Account` information from it. This is supposed to support the most relevant
+ * Represents a generic function that takes an `Any` encoded account from the chain
+ * and extracts some common `Account` information from it.
+ */
+export type AccountParser = (any: Any) => Account;
+
+/**
+ * Basic implementation of AccountParser. This is supposed to support the most relevant
  * common Cosmos SDK account types. If you need support for exotic account types,
  * you'll need to write your own account decoder.
  */
 export function accountFromAny(input: Any): Account {
-    const { typeUrl, value } = input;
+  const { typeUrl, value } = input;
 
-    switch (typeUrl) {
-        // auth
+  switch (typeUrl) {
+
+    // auth
     case "/cosmos.auth.v1beta1.BaseAccount":
-        return accountFromBaseAccount(BaseAccount.decode(value));
-    case "/cosmos.auth.v1beta1.ModuleAccount":
-    {
-        const baseAccount = ModuleAccount.decode(value).baseAccount;
-        return accountFromBaseAccount(baseAccount!);
+      return accountFromBaseAccount(BaseAccount.decode(value));
+    case "/cosmos.auth.v1beta1.ModuleAccount": {
+      const baseAccount = ModuleAccount.decode(value).baseAccount;
+      assert(baseAccount);
+      return accountFromBaseAccount(baseAccount!);
     }
 
     // vesting
-    case "/cosmos.vesting.v1beta1.BaseVestingAccount":
-    {
-        const baseAccount = BaseVestingAccount.decode(value)?.baseAccount;
-        return accountFromBaseAccount(baseAccount!);
+    case "/cosmos.vesting.v1beta1.BaseVestingAccount": {
+      const baseAccount = BaseVestingAccount.decode(value)?.baseAccount;
+      assert(baseAccount);
+      return accountFromBaseAccount(baseAccount);
     }
-    case "/cosmos.vesting.v1beta1.ContinuousVestingAccount":
-    {
-        const baseAccount = ContinuousVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
-        return accountFromBaseAccount(baseAccount!);
+    case "/cosmos.vesting.v1beta1.ContinuousVestingAccount": {
+      const baseAccount = ContinuousVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
+      assert(baseAccount);
+      return accountFromBaseAccount(baseAccount);
     }
-    case "/cosmos.vesting.v1beta1.DelayedVestingAccount":
-    {
-        const baseAccount = DelayedVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
-        return accountFromBaseAccount(baseAccount!);
+    case "/cosmos.vesting.v1beta1.DelayedVestingAccount": {
+      const baseAccount = DelayedVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
+      assert(baseAccount);
+      return accountFromBaseAccount(baseAccount);
     }
-    case "/cosmos.vesting.v1beta1.PeriodicVestingAccount":
-    {
-        const baseAccount = PeriodicVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
-        return accountFromBaseAccount(baseAccount!);
-    }
-
-    case "/ethermint.types.v1.EthAccount":
-    {
-        const baseAccount = ModuleAccount.decode(value).baseAccount;
-        return accountFromBaseAccount(baseAccount!);
+    case "/cosmos.vesting.v1beta1.PeriodicVestingAccount": {
+      const baseAccount = PeriodicVestingAccount.decode(value)?.baseVestingAccount?.baseAccount;
+      assert(baseAccount);
+      return accountFromBaseAccount(baseAccount);
     }
 
     default:
-        throw new Error(`Unsupported type: '${typeUrl}'`);
-    }
+      throw new Error(`Unsupported type: '${typeUrl}'`);
+  }
 }
