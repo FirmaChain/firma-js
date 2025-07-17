f14fc00e19eaf2a061c9ebeb948defae01118e08
thismars <yhklip990@gmail.com>
2025-07-17 13:20:16 +0900

    refactor: add memo parameter to transfer methods and update timeoutTimestamp type

--- sdk/FirmaIbcService.ts
+++ sdk/FirmaIbcService.ts
@@ -3,10 +3,9 @@ import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import { FirmaWalletService } from "./FirmaWalletService";
 import { FirmaConfig } from "./FirmaConfig";
 import { DefaultTxMisc, FirmaUtil, getSignAndBroadcastOption } from "./FirmaUtil";
-import { BroadcastTxResponse } from "./firmachain/common/stargateclient";
 import { ClientState, IbcQueryClient, IbcTxClient, TxMisc } from "./firmachain/ibc";
-import Long from "long";
 import { Height } from "cosmjs-types/ibc/core/client/v1/client";
+import { DeliverTxResponse } from "@cosmjs/stargate";
 
 export class FirmaIbcService {
 
@@ -23,10 +22,11 @@ export class FirmaIbcService {
         amount: string,
         receiver: string,
         timeoutHeight: Height,
-        timeoutTimestamp: Long, txMisc: TxMisc = DefaultTxMisc):
-        Promise<number>  {
+        timeoutTimestamp: bigint,
+        memo: string = "",
+        txMisc: TxMisc = DefaultTxMisc): Promise<number>  {
         try {
-            const txRaw = await this.getSignedTxTransfer(wallet, sourcePort, sourceChannel, denom, amount, receiver, timeoutHeight, timeoutTimestamp, txMisc);
+            const txRaw = await this.getSignedTxTransfer(wallet, sourcePort, sourceChannel, denom, amount, receiver, timeoutHeight, timeoutTimestamp, memo, txMisc);
             return await FirmaUtil.estimateGas(txRaw);
 
         } catch (error) {
@@ -42,10 +42,11 @@ export class FirmaIbcService {
         amount: string,
         receiver: string,
         timeoutHeight: Height,
-        timeoutTimestamp: Long, txMisc: TxMisc = DefaultTxMisc):
-        Promise<BroadcastTxResponse> {
+        timeoutTimestamp: bigint,
+        memo: string = "",
+        txMisc: TxMisc = DefaultTxMisc): Promise<DeliverTxResponse> {
         try {
-            const txRaw = await this.getSignedTxTransfer(wallet, sourcePort, sourceChannel, denom, amount, receiver, timeoutHeight, timeoutTimestamp, txMisc);
+            const txRaw = await this.getSignedTxTransfer(wallet, sourcePort, sourceChannel, denom, amount, receiver, timeoutHeight, timeoutTimestamp, memo, txMisc);
 
             const txClient = new IbcTxClient(wallet, this.config.rpcAddress);
             return await txClient.broadcast(txRaw);
@@ -63,14 +64,18 @@ export class FirmaIbcService {
         amount: string,
         receiver: string,
         timeoutHeight: Height,
-        timeoutTimestamp: Long,
+        timeoutTimestamp: bigint,
+        memo: string = "",
         txMisc: TxMisc = DefaultTxMisc): Promise<TxRaw> {
         try {
             const address = await wallet.getAddress();
 
-            const message = IbcTxClient.msgTransfer({ sourcePort: sourcePort, sourceChannel: sourceChannel, sender: address, receiver: receiver, 
-                token: {denom: denom, amount: amount},
-                timeoutHeight: timeoutHeight, timeoutTimestamp: timeoutTimestamp });
+            const message = IbcTxClient.msgTransfer({
+                sourcePort: sourcePort, sourceChannel: sourceChannel, sender: address, receiver: receiver,
+                token: { denom: denom, amount: amount },
+                timeoutHeight: timeoutHeight, timeoutTimestamp: timeoutTimestamp,
+                memo: memo
+            });
 
             const ibcTxClient = new IbcTxClient(wallet, this.config.rpcAddress);
             return await ibcTxClient.sign([message], getSignAndBroadcastOption(this.config.denom, txMisc));
