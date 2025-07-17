63ee23993f7f93f1ec565d7ca5851af05b1507cd
thismars <yhklip990@gmail.com>
2025-07-17 14:13:43 +0900

    refactor: update import paths and add MsgCancelProposal type for governance

--- sdk/firmachain/common/CommonTxClient.ts
+++ sdk/firmachain/common/CommonTxClient.ts
@@ -4,6 +4,9 @@ import { MsgFundCommunityPool, MsgSetWithdrawAddress, MsgWithdrawDelegatorReward
 import { MsgDeposit, MsgSubmitProposal, MsgVote } from "cosmjs-types/cosmos/gov/v1beta1/tx";
 import { MsgBeginRedelegate, MsgCreateValidator, MsgDelegate, MsgEditValidator, MsgUndelegate } from "cosmjs-types/cosmos/staking/v1beta1/tx";
 import { MsgStoreCode, MsgInstantiateContract, MsgExecuteContract, MsgUpdateAdmin, MsgClearAdmin, MsgMigrateContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+// temporarly using kintsugi-tech/cosmjs-types - this will be returned to original cosmjs-types after the PR is merged
+import { MsgCancelProposal } from "@kintsugi-tech/cosmjs-types/cosmos/gov/v1/tx";
+
 import { FirmaWalletService } from "../../FirmaWalletService";
 import { MsgExec, MsgGrant, MsgRevoke } from "../authz/AuthzTxTypes";
 import { MsgCreateContractFile, MsgAddContractLog } from "../contract/ContractTxTypes";
@@ -11,6 +14,7 @@ import { MsgGrantAllowance, MsgRevokeAllowance } from "../feegrant/FeeGrantTxTyp
 import { MsgTransfer, MsgMint, MsgBurn } from "../nft/NftTxTypes";
 import { MsgCreateToken, MsgUpdateTokenURI } from "../token/TokenTxTypes";
 import { ITxClient } from "./ITxClient";
+
 
 const types = [
     ["/cosmos.authz.v1beta1.MsgExec", MsgExec],
@@ -20,8 +24,8 @@ const types = [
     ["/cosmos.bank.v1beta1.MsgSend", MsgSend],
     ["/cosmos.bank.v1beta1.MsgMultiSend", MsgMultiSend],
 
-    ["/firmachain.firmachain.contract.MsgCreateContractFile", MsgCreateContractFile],
-    ["/firmachain.firmachain.contract.MsgAddContractLog", MsgAddContractLog],
+    ["/firmachain.contract.MsgCreateContractFile", MsgCreateContractFile],
+    ["/firmachain.contract.MsgAddContractLog", MsgAddContractLog],
 
     ["/cosmwasm.wasm.v1.MsgStoreCode", MsgStoreCode],
     ["/cosmwasm.wasm.v1.MsgInstantiateContract", MsgInstantiateContract],
@@ -42,12 +46,13 @@ const types = [
     ["/cosmos.gov.v1beta1.MsgDeposit", MsgDeposit],
     ["/cosmos.gov.v1beta1.MsgSubmitProposal", MsgSubmitProposal],
     ["/cosmos.gov.v1beta1.MsgVote", MsgVote],
+    ["/cosmos.gov.v1.MsgCancelProposal", MsgCancelProposal],
 
     ["/ibc.applications.transfer.v1.MsgTransfer", MsgTransfer],
 
-    ["/firmachain.firmachain.nft.MsgTransfer", MsgTransfer],
-    ["/firmachain.firmachain.nft.MsgMint", MsgMint],
-    ["/firmachain.firmachain.nft.MsgBurn", MsgBurn],
+    ["/firmachain.nft.MsgTransfer", MsgTransfer],
+    ["/firmachain.nft.MsgMint", MsgMint],
+    ["/firmachain.nft.MsgBurn", MsgBurn],
 
     ["/cosmos.staking.v1beta1.MsgDelegate", MsgDelegate],
     ["/cosmos.staking.v1beta1.MsgUndelegate", MsgUndelegate],
@@ -55,10 +60,10 @@ const types = [
     ["/cosmos.staking.v1beta1.MsgEditValidator", MsgEditValidator],
     ["/cosmos.staking.v1beta1.MsgCreateValidator", MsgCreateValidator],
 
-    ["/firmachain.firmachain.token.MsgCreateToken", MsgCreateToken],
-    ["/firmachain.firmachain.token.MsgUpdateTokenURI", MsgUpdateTokenURI],
-    ["/firmachain.firmachain.token.MsgMint", MsgMint],
-    ["/firmachain.firmachain.token.MsgBurn", MsgBurn]
+    ["/firmachain.token.MsgCreateToken", MsgCreateToken],
+    ["/firmachain.token.MsgUpdateTokenURI", MsgUpdateTokenURI],
+    ["/firmachain.token.MsgMint", MsgMint],
+    ["/firmachain.token.MsgBurn", MsgBurn]
     
 ];
 
