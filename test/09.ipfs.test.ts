8086991b67cda8b599b76bfab8c11787635257e5
thismars <yhklip990@gmail.com>
2026-04-21 16:00:08 +0900

    test: update IPFS test cases for proposal JSON and buffer upload

--- test/09.ipfs.test.ts
+++ test/09.ipfs.test.ts
@@ -11,22 +11,34 @@ describe('[09. IPFS Test]', () => {
 		firma = new FirmaSDK(TestChainConfig);
 	})
 
-	it('ipfs test', async () => {
+	it('ipfs add proposal json', async () => {
+		
+		// JSON (Proposal)
+		const proposalJson = `{
+			"title": "FirmaChain CommunityPoolSpend Proposal",
+			"authors": [
+				"firma1nssuz67am2uwc2hjgvphg0fmj3k9l6cx65ux9u"
+			],
+			"summary": "Summary Section",
+			"details": "Details Section",
+			"proposal_forum_url": "explorer-testnet.firmachain.dev",
+			"vote_option_context": "Yes, No, Vote, Abstain"
+		}`;
+		let hash = await firma.Ipfs.addJson(proposalJson);
+		expect(hash).to.equal("QmT7eJ668Hr6LbUsyAESenV6QRW84Rz3mGkLcFhs1rRxa7");
 
-		let hash = await firma.Ipfs.addJson("hello world");
-		expect(hash).to.equal("Qmf412jQZiuVUtdgnB36FXFX7xg5V6KEbSJ4dpQuhkLyfD");
-
+		// Buffer
 		const arrBuf = new ArrayBuffer(1000);
 		hash = await firma.Ipfs.addBuffer(arrBuf);
+		expect(hash).to.equal("QmT7eJ668Hr6LbUsyAESenV6QRW84Rz3mGkLcFhs1rRxa7");
 		let url = firma.Ipfs.getURLFromHash(hash);
-
 		let srcUrl = firma.Config.ipfsWebApiAddress + "/ipfs/QmVRqQTWMy2gNtNd8i9ugz8STaoZmFGYg6fn5YyEBHp9Be";
 		expect(srcUrl).to.equal(url);
 
+		// File (Image)
 		hash = await firma.Ipfs.addFile("./test/sample/test-bear.jpg");
 		url = firma.Ipfs.getURLFromHash(hash);
 		srcUrl = firma.Config.ipfsWebApiAddress + "/ipfs/QmYsezxzunake9EmyoU4HsWKEyHQLgE3syTEpTSQEhNChA";
-
 		expect(srcUrl).to.equal(url);
 	});
 });
